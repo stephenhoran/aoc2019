0733570a083fd50a68c6aee00951fb1d399f6d42
shoran <shoran@galileosuite.com>
2019-12-01 22:42:33 -0800

    Commit Day 2 Part 2

---
+++ Day-2/2/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"../../commons"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	var original []int
+
+	index := 0
+	f := commons.OpenFile("input.txt")
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		i := strings.Split(scanner.Text(), ",")
+		for _, n := range i {
+			num, _ := strconv.Atoi(n)
+			original = append(original, num)
+		}
+	}
+
+	t := time.Now()
+
+	for v := 0; v <= 99; v++ {
+		for n := 0; n <= 99; n++ {
+			input := make([]int, len(original))
+			copy(input, original)
+			intCode(index, input, n, v)
+		}
+	}
+
+	fmt.Printf("Time to work: %v", time.Since(t))
+}
+
+func intCode(index int, input []int, noun int, verb int) {
+	input[1] = noun
+	input[2] = verb
+
+	opCode := input[index]
+	pos1 := input[index+1]
+	pos2 := input[index+2]
+	store := input[index+3]
+
+	if opCode == 99 {
+		if input[0] == 19690720 {
+			fmt.Printf("Noun: %d - Verb: %d\n", noun, verb)
+			fmt.Println(100*noun + verb)
+			os.Exit(1)
+		}
+		return
+	}
+
+	if opCode == 1 {
+		value := input[pos1] + input[pos2]
+		input[store] = value
+	}
+
+	if opCode == 2 {
+		value := input[pos1] * input[pos2]
+		input[store] = value
+	}
+
+	intCode(index+4, input, noun, verb)
+}
